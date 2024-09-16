06953dd2c28660dbf01cbe1997b2dc9862094bb3
kush desai <kush@artiumacademy.com>
2024-09-16 18:21:40 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
     <div className="container">
       {
         updatedCountries && updatedCountries?.map((country,idx)=>(
-          <div className={`countryContainer ${country}Card`} key={idx}>
+          <div className="countryCard" key={idx}>
             <img src={country?.flags?.png} alt={country?.name?.common} className='countryImg'/>
             <p>{country?.name?.common}</p>
           </div>
